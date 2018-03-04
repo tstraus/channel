f4601d11c58cf7e0bf46e1af735f3edce7c0d952
Tucke =r Strausbaugh <tckrsstrsbgh@gmail.com>
2018-03-04 13:19:12 -0500

    tabs to spaces...

--- channel.h
+++ channel.h
@@ -4,72 +4,72 @@
 
 namespace tstraus
 {
-	template <typename T>
-	class channel
-	{
-	public:
-		channel() : m_closed(false),
-			waitLambda([&]() { return m_closed || !q.empty(); }) {}
+    template <typename T>
+    class channel
+    {
+    public:
+        channel() : m_closed(false),
+            waitLambda([&]() { return m_closed || !q.empty(); }) {}
 
-		void close()
-		{
-			std::unique_lock<std::mutex> lock(m);
+        void close()
+        {
+            std::unique_lock<std::mutex> lock(m);
 
-			m_closed = true;
-			cv.notify_all();
-		}
+            m_closed = true;
+            cv.notify_all();
+        }
 
-		bool closed()
-		{
-			std::unique_lock<std::mutex> lock(m);
+        bool closed()
+        {
+            std::unique_lock<std::mutex> lock(m);
 
-			return m_closed;
-		}
+            return m_closed;
+        }
 
-		void put(const T& input)
-		{
-			std::unique_lock<std::mutex> lock(m);
+        void put(const T& input)
+        {
+            std::unique_lock<std::mutex> lock(m);
 
-			if (m_closed)
-				throw std::logic_error("can't put in a closed channel");
+            if (m_closed)
+                throw std::logic_error("can't put in a closed channel");
 
-			q.push(input);
-			cv.notify_one();
-		}
+            q.push(input);
+            cv.notify_one();
+        }
 
-		bool get(T& output, bool wait = false)
-		{
-			std::unique_lock<std::mutex> lock(m);
+        bool get(T& output, bool wait = false)
+        {
+            std::unique_lock<std::mutex> lock(m);
 
-			if (wait)
-				cv.wait(lock, waitLambda);
+            if (wait)
+                cv.wait(lock, waitLambda);
 
-			if (q.empty())
-				return false;
+            if (q.empty())
+                return false;
 
-			output = q.front();
-			q.pop();
+            output = q.front();
+            q.pop();
 
-			return true;
-		}
+            return true;
+        }
 
-		void operator<<(const T& input)
-		{
-			put(input);
-		}
+        void operator<<(const T& input)
+        {
+            put(input);
+        }
 
-		T& operator>>(T& output)
-		{
-			get(output, true);
+        T& operator>>(T& output)
+        {
+            get(output, true);
 
-			return output;
-		}
+            return output;
+        }
 
-	private:
-		std::queue<T> q;
-		std::mutex m;
-		std::condition_variable cv;
-		std::function<bool()> waitLambda;
-		bool m_closed;
-	};
+    private:
+        std::queue<T> q;
+        std::mutex m;
+        std::condition_variable cv;
+        std::function<bool()> waitLambda;
+        bool m_closed;
+    };
 }
--- test.cpp
+++ test.cpp
@@ -7,32 +7,32 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	tstraus::channel<string> c;
+    tstraus::channel<string> c;
 
-	thread t1([&]()
-	{
-		c << "asdf"; // send to channel
+    thread t1([&]()
+    {
+        c << "asdf"; // send to channel
 
-		// Not normal to read and write from the same thread
-		// so we'll sleep so we don't read our own message
-		this_thread::sleep_for(chrono::milliseconds(1));
+        // Not normal to read and write from the same thread
+        // so we'll sleep so we don't read our own message
+        this_thread::sleep_for(chrono::milliseconds(1));
 
-		string o;
-		c >> o; // receive from channel
-		cout << o << endl;
-	});
+        string o;
+        c >> o; // receive from channel
+        cout << o << endl;
+    });
 
-	thread t2([&]()
-	{
-		string o;
-		c >> o;
-		cout << o << endl;
+    thread t2([&]()
+    {
+        string o;
+        c >> o;
+        cout << o << endl;
 
-		c << "fdsa";
-	});
+        c << "fdsa";
+    });
 
-	t1.join();
-	t2.join();
+    t1.join();
+    t2.join();
 
-	return 0;
+    return 0;
 }
